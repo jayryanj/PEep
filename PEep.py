110a4d6aafdf786576eb84115b0b83e1efe5acda
jayryanj <43054912+jayryanj@users.noreply.github.com>
2019-11-28 17:38:44 -1000

    Started collect function
    Created a collect() function that so far collects some basic data from the DOS, File, and Optional header and adds to a "data" dictionary. Refactored entropy function to just handle one section at a time.

--- PEep.py
+++ PEep.py
@@ -9,6 +9,7 @@ import os
 import sys
 import math
 import pefile
+import datetime
 from argparse import *
 
 is_verbose = False
@@ -68,7 +69,7 @@ def arguments():
         help="Requires the [-s, --section] option. Hex dump the section to a specified file in the "
              "working directory"
     )
-    # Return the parsed arguments
+    # Return the parsed arguments as a dictionary
     return vars(parser.parse_args())
 
 
@@ -130,7 +131,6 @@ def calculate(filename, check_file=False):
     if is_verbose:
         print("* Checking section entropies...")
     file = pefile.PE(filename, fast_load=False)
-    section_count = file.FILE_HEADER.NumberOfSections
 
     for section in file.sections:
         section_name = section.Name.decode("utf-8")
@@ -167,6 +167,36 @@ def calculate(filename, check_file=False):
     return section_entropies
 
 
+def get_entropy(section):
+    section_name = section.Name.decode("utf-8")
+    if is_verbose:
+        print("* Calculating for %s..." % section_name)
+    data = section.get_data()
+    length = len(data)
+    freq_list = []
+
+    if is_verbose:
+        print("* Calculating byte frequencies...")
+    for i in range(0, 256):
+        counter = 0
+
+        for byte in data:
+            if byte == i:
+                counter += 1
+
+        freq_list.append(float(counter) / length)
+
+    entropy = 0.0
+    if is_verbose:
+        print("* Calculating Shannon entropy for %s..." % section_name)
+    for frequency in freq_list:
+        if frequency > 0:
+            entropy = entropy + frequency * math.log(frequency, 2)
+
+    entropy = -entropy
+    return entropy
+
+
 def print_entropies(section_entropies, check_file=False):
     """
     Prints out the entropies.
@@ -184,32 +214,86 @@ def print_entropies(section_entropies, check_file=False):
             print("%s: %f" % (entropy[0], entropy[1]))
 
 
+def collect(filename):
+    """
+    Collects the data from the PE format of the specified filename. It assumes that the file already exists in the
+    working directory.
+    :param filename: String representing the file's name
+    :return: Dictionary containing all collected data
+    """
+    data = {}
+    file = pefile.PE(filename, fast_load=False)
+
+    if file.DOS_HEADER.dump_dict().get('e_magic').get('Value') != 23117:
+        with open(filename, mode='rb') as file:
+            file_data = file.read()
+            signature = file_data[:2].decode("utf-8")
+            if signature != 'MZ':
+                pass
+            else:
+                raise pefile.PEFormatError("File: %s doesn't contain 'MZ' magic value" % filename)
+
+    data.update({'Section Count': file.FILE_HEADER.NumberOfSections})
+    data.update({'Page Count': file.DOS_HEADER.dump_dict().get('e_cp').get('Value')})
+    data.update({'Time': str(datetime.datetime.fromtimestamp(file.FILE_HEADER.TimeDateStamp))})
+
+    # TODO: Add more machines (MIPS, RISC-V, Hitachi)
+    machine = file.FILE_HEADER.dump_dict().get('Machine').get('Value')
+    machines_dict = {34404: "x86", 332: "Intel 386", 448: "ARM", 43620: "ARM64", 3772: "EFI"}
+    if machines_dict.get(machine):
+        data.update({'Machine': machines_dict.get(machine)})
+    else:
+        data.update({'Machine': "Unknown"})
+
+    data.update({'Entry Point': file.OPTIONAL_HEADER.dump_dict().get('AddressOfEntryPoint').get('Value')})
+
+    # Sets the subsystem by comparing returned integer from OPTIONAL_HEADER to a predetermined dictionary of subsystems
+    subsystem = file.OPTIONAL_HEADER.dump_dict().get('Subsystem').get('Value')
+    subsystems_dict = {1: "Native", 2: "Windows GUI", 3: "Windows CUI", 5: "OS/2 CUI", 7: "POSIX CUI",
+                       8: "Native Windows", 9: "Windows CE", 10: "EFI Application", 16: "Windows Boot Application"}
+    if subsystems_dict.get(subsystem):
+        data.update({'Subsystem': subsystems_dict.get(subsystem)})
+    else:
+        data.update({'Subsystem': "Unknown"})
+
+    for section in file.sections:
+        section_name = section.Name.decode("utf-8").rstrip('\x00')
+        data.update({
+            section_name: {
+                'name': section_name,
+                'entropy': round(get_entropy(section), 5),
+                'address': hex(section.VirtualAddress),
+            }
+        })
+    for value in file.OPTIONAL_HEADER.dump_dict():
+        print("%s - %s" % (value, str(file.OPTIONAL_HEADER.dump_dict().get(value))))     # Use for testing
+    return data
+
+
 # Main function
 def main():
     global is_verbose
     ascii_art = " ____  _____       \n|  _ \\| ____|___ _ __  \n| |_) |  _| / _ \\ '_ \\ \n|  __/| |__|  __/ |_) |\n" \
                 "|_|   |_____\\___| .__/ \n                |_|    "
 
-    print(ascii_art)
+    # print(ascii_art)                # REMOVE # TO PRINT THE ASCII ART
 
     # Grabs command-line arguments
     args = arguments()
     print(args)                         # DELETE THIS
     filename = args.get('file')[0]
-    section = args.get('section')[0]
+    section = args.get('section')
     is_verbose = args.get('verbose')
 
     # TODO: Call functions here based on args
     if os.path.exists(filename) and os.path.isfile(filename):
-        pass
-    if section:
-        pass
-    if args.get('threat'):
-        pass
-    if args.get('dump'):
-        pass
-    else:
-        pass
+        data = collect(filename)
+        if section:
+            pass
+        if args.get('threat'):
+            pass
+        if args.get('dump'):
+            pass
 
 
 if __name__ == "__main__":
