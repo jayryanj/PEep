e7d18ee8c597695859dbbb3c17259509840b225d
jayryanj <43054912+jayryanj@users.noreply.github.com>
2019-12-02 10:34:26 -1000

    Finished data collection
    Finished data collection and implemented printing in main function

--- PEep.py
+++ PEep.py
@@ -30,6 +30,7 @@ def arguments():
         "file": ["-f", "--file"],
         "threat": ["-t", "--threat"],
         "dump": ["-d", "--dump"],
+        "imports": ["-i", "--imports"]
     }
 
     parser = ArgumentParser(description=description, usage="PEep.py [OPTIONS] [-f, --file] [File Name]")
@@ -62,6 +63,11 @@ def arguments():
         action="store_true"
     )
     parser.add_argument(
+        options.get("imports")[0],
+        options.get("imports")[1],
+        action="store_true"
+    )
+    parser.add_argument(
         options.get("dump")[0],
         options.get("dump")[1],
         nargs=2,
@@ -73,101 +79,12 @@ def arguments():
     return vars(parser.parse_args())
 
 
-def calculate(filename, check_file=False):
-    """
-    Calculates the entropies of the sections or entire file for the given PE file name. The file must
-    be within the same working directory.
-    :param filename: A string to the file within the working directory
-    :param check_file: Whether or not to check the entropy of the entire file
-    :return: List of 2-tuple with the values: (section name, entropy) for each section.
-    """""
-    section_entropies = []
-    global is_verbose
-
-    if is_verbose:
-        print("* Loading %s*" % filename)
-    with open(filename, mode='rb') as file:
-
-        file_data = file.read()
-        signature = file_data[:2].decode("utf-8")
-
-        # TODO: Create a thorough check for PE format
-        if signature != "MZ":
-            print("* ERROR - File does not match the PE format - Missing 0x4D 0x5A (MZ) signature")
-            sys.exit()
-
-        # File entropy calculation
-        if check_file:
-            if is_verbose:
-                print("* Checking file entropy (--file)...")
-            size = len(file_data)
-            freq_list = []
-
-            if is_verbose:
-                print("* Calculating byte frequencies...")
-            for i in range(0, 256):
-                counter = 0
-
-                for byte in file_data:
-                    if byte == i:
-                        counter += 1
-
-                freq_list.append(float(counter) / size)
-
-            entropy = 0.0
-
-            if is_verbose:
-                print("* Calculating Shannon entropy for %s..." % filename)
-            for frequency in freq_list:
-                if frequency > 0:
-                    entropy = entropy + frequency * math.log(frequency, 2)
-
-            entropy = -entropy
-            section_entropies.append((filename, entropy))
-            if is_verbose:
-                print("* File entropy calculating complete")
-
-    # Section entropy calculations
-    if is_verbose:
-        print("* Checking section entropies...")
-    file = pefile.PE(filename, fast_load=False)
-
-    for section in file.sections:
-        section_name = section.Name.decode("utf-8")
-        if is_verbose:
-            print("* Calculating for %s..." % section_name)
-        section_address = hex(section.VirtualAddress)  # Will be used in future code
-        data = section.get_data()
-        length = len(data)
-        freq_list = []
-
-        if is_verbose:
-            print("* Calculating byte frequencies...")
-        for i in range(0, 256):
-            counter = 0
-
-            for byte in data:
-                if byte == i:
-                    counter += 1
-
-            freq_list.append(float(counter)/length)
-
-        entropy = 0.0
-        if is_verbose:
-            print("* Calculating Shannon entropy for %s..." % section_name)
-        for frequency in freq_list:
-            if frequency > 0:
-                entropy = entropy + frequency * math.log(frequency, 2)
-
-        entropy = -entropy
-        section_entropies.append((section_name, entropy))
-        if is_verbose:
-            print("* %s entropy calculating complete" % section_name)
-
-    return section_entropies
-
-
 def get_entropy(section):
+    """
+    Calculates the entropy of the given PE section
+    :param section: PE section
+    :return: The entropy as a floating-point number
+    """
     section_name = section.Name.decode("utf-8")
     if is_verbose:
         print("* Calculating for %s..." % section_name)
@@ -197,23 +114,6 @@ def get_entropy(section):
     return entropy
 
 
-def print_entropies(section_entropies, check_file=False):
-    """
-    Prints out the entropies.
-    :param section_entropies: A tuple consisting of entropies and their respective names
-    :param check_file: Whether or not the user chose to check the entropy of the entire file
-    :return:
-    """
-    if check_file:
-        print('File entropy for "%s" : %f' % (section_entropies[0][0], section_entropies[0][1]))
-    print("Section Entropies:")
-    for entropy in section_entropies:
-        if check_file and entropy == section_entropies[0]:
-            pass
-        else:
-            print("%s: %f" % (entropy[0], entropy[1]))
-
-
 def collect(filename):
     """
     Collects the data from the PE format of the specified filename. It assumes that the file already exists in the
@@ -238,8 +138,9 @@ def collect(filename):
     data.update({'Time': str(datetime.datetime.fromtimestamp(file.FILE_HEADER.TimeDateStamp))})
 
     # TODO: Add more machines (MIPS, RISC-V, Hitachi)
+    # Sets the machine by comparing returned integer from FILE_HEADER to a predetermined dictionary of machines
     machine = file.FILE_HEADER.dump_dict().get('Machine').get('Value')
-    machines_dict = {34404: "x86", 332: "Intel 386", 448: "ARM", 43620: "ARM64", 3772: "EFI"}
+    machines_dict = {34404: "x86-64", 332: "Intel i386", 448: "ARM", 43620: "ARM64", 3772: "EFI"}
     if machines_dict.get(machine):
         data.update({'Machine': machines_dict.get(machine)})
     else:
@@ -256,17 +157,22 @@ def collect(filename):
     else:
         data.update({'Subsystem': "Unknown"})
 
+    # Gets information on each section including name, entropy, address, virtual size, and raw size
+    sections_dict = {}
     for section in file.sections:
         section_name = section.Name.decode("utf-8").rstrip('\x00')
-        data.update({
+        sections_dict.update({
             section_name: {
                 'name': section_name,
                 'entropy': round(get_entropy(section), 5),
                 'address': hex(section.VirtualAddress),
+                'Virtual Size': hex(section.Misc_VirtualSize),
+                'Raw Size': hex(section.SizeOfRawData)
             }
         })
-    for value in file.OPTIONAL_HEADER.dump_dict():
-        print("%s - %s" % (value, str(file.OPTIONAL_HEADER.dump_dict().get(value))))     # Use for testing
+    # Updates the data dictionary with a sections dictionary
+    data.update({'Sections': sections_dict})
+
     return data
 
 
@@ -276,11 +182,11 @@ def main():
     ascii_art = " ____  _____       \n|  _ \\| ____|___ _ __  \n| |_) |  _| / _ \\ '_ \\ \n|  __/| |__|  __/ |_) |\n" \
                 "|_|   |_____\\___| .__/ \n                |_|    "
 
-    # print(ascii_art)                # REMOVE # TO PRINT THE ASCII ART
+    print(ascii_art)
 
     # Grabs command-line arguments
     args = arguments()
-    print(args)                         # DELETE THIS
+    # print(args)                         # TESTING
     filename = args.get('file')[0]
     section = args.get('section')
     is_verbose = args.get('verbose')
@@ -294,6 +200,22 @@ def main():
             pass
         if args.get('dump'):
             pass
+        if args.get('imports'):
+            pass
+
+        print("============================[File]============================")
+        print('File Name : "%s"' % filename)
+        for value in data:
+            if value != "Sections":
+                print("%s : %s" % (value, data.get(value)))
+            else:
+                print("==========================[Sections]==========================")
+                for section in data.get(value):
+                    print('%s:' % section)
+                    print('  Address : %s' % data.get(value).get(section).get('address'))
+                    print('  Entropy : %s' % data.get(value).get(section).get('entropy'))
+                    print('  Raw Size : %s' % data.get(value).get(section).get('Raw Size'))
+                    print('  Virtual Size : %s' % data.get(value).get(section).get('Virtual Size'))
 
 
 if __name__ == "__main__":
